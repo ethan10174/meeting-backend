aeac846adff4e16b9c8c4c1640c5f2d46603eb9e
ethan10174 <ethanallison91307@gmail.com>
2026-04-22 19:45:51 -0400

    Add Vercel URL to CORS

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from openai import OpenAI
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://meeting-frontend-ashy.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
